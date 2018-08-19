e05b74908f8ca38ddf4b730954c3be50e16a62e0
SharkFin <sharkfinpro@hotmail.com>
2018-08-19 11:17:38 -0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const server = require('./source/server.js');
-const config = require('./config.js');
 
-const diepServer = new server(config);
+const diepServer = new server(require('./config.js'));
 diepServer.init();
